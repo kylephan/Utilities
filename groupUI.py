870f08e96d4208b8df3cb5f575cbb00ef368997d
kylephan <31226885+kylephan@users.noreply.github.com>
2018-11-23 12:49:03 +1300

    Update groupUI.py

--- groupUI.py
+++ groupUI.py
@@ -1,5 +1,13 @@
+'''
+Author - Kyle Phan
+Version - 0.1
+Date - 08/11/2018
+Description - Creat empty group to act as a holder for objects
+'''
+
 import maya.cmds as mc
 
+## for controls
 def groupControls(*args):
     objects = mc.ls( sl = True, o = True, sn = True )
     
@@ -15,7 +23,8 @@ def groupControls(*args):
         mc.setAttr( grp + '.rotateZ' , 0 )
         
         mc.parent( obj , grp )
-        
+
+## for joints
 def groupJoints(*args):
     objects = mc.ls( sl = True, o = True, sn = True )
     
@@ -31,7 +40,8 @@ def groupJoints(*args):
         mc.setAttr( grp + '.rotateZ' , 0 )
         
         mc.parent( obj , grp )
-        
+
+## UI starts here
 windowID = 'groupUtilityUI'
 
 if mc.window( windowID, exists = True):
